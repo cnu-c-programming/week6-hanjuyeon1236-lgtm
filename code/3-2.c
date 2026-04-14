5eac5abb4e3b25ecad850c69642665c75222f783
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-14 23:58:44 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -3,7 +3,7 @@
 int main() {
   int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
-  for(int i = 0; i < 2;; i++) {
+  for(int i = 0; i < 2; i++) {
      for(int j = 0; j < 3; j++) {
        printf("%p\n", (void*)&arr[i][j]);
      }
